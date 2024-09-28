dcbda96fc73b313de7d1a0aa42a1f47159a57ca6
shayanxmoradi <shayan.moradi92@gmail.com>
2024-09-28 18:17:49 +0200

    init

---
+++ src/main/java/org/example/springcw/SpringCwApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springcw/SpringCwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
